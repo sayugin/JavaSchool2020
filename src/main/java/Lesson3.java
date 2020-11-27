27295625a0fd11e1983958534ae143e47a0236c4
Goodwin <yugin.sa@gmail.com>
2020-11-27 18:21:13 +0300

    Добавил поля для хранения размера массива и конечного элемента. Тем самым убрал очевидные косяки начальной реализации, когда для добавления нового узла нужно было пробегать по всему списку чтобы добраться до последнего элемента. Для тренировки поменял тип хранимого объекта с Object'а на дженерик. Ну и непосредственно реализовал то, что просили, т.е. метод get.

--- src/main/java/Lesson3.java
+++ src/main/java/Lesson3.java
@@ -1,64 +1,94 @@
 public class Lesson3 {
+    public static void main(String[] args) {
+        final int COUNT = 10_000_000;
+        final int STEP = 2_000_000;
 
-}
+        for (int i = 0; i < COUNT / STEP; i++) {
+            int elementCount = STEP * (i + 1);
+            LList<Integer> list = new LList<>();
+            long t1 = System.currentTimeMillis();
+            for (int j = 0; j < elementCount; j++)
+                list.add(j + 1);
+            long t2 = System.currentTimeMillis();
+            System.out.printf(String.format("Кол-во элементов: %d. Генерация списка: %d%n", elementCount, t2 - t1));
 
-class Node {
-    public Node(Object data) {
-        this.data = data;
-    }
-    Object data;
-    Node next;
-}
-
-class LList {
-    private Node root;
-
-    public void add(Object item) {
-        Node tmpItem = new Node(item);
-        Node lastItem = findLast();
-
-        if (lastItem != null) {
-            lastItem.next = tmpItem;
-        } else {
-            root = tmpItem;
+            t1 = System.currentTimeMillis();
+            Integer data = list.get(elementCount - 2);
+            t2 = System.currentTimeMillis();
+            System.out.printf("Значение и время извлечения предпоследнего элемента: %d - %d%n", data, t2 - t1);
+            System.out.println("*******************************************************************");
         }
     }
+}
 
-    public Object get(int id) {
-        // TODO
-        return null;
+class Node<T> {
+
+    T data;
+    Node<T> next;
+
+    public Node(T data) {
+        this.data = data;
+    }
+
+    public boolean hasNext(){
+        return next != null;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+}
+
+class LList<T> {
+
+
+    private Node<T> first;
+    private Node<T> last;
+    private int size;
+
+    public LList() {
+        this.size = 0;
+    }
+
+    public void add(T item) {
+        Node<T> newNode = new Node<>(item);
+        if (this.last != null)
+            this.last.next = newNode;
+        else
+            this.first = newNode;
+
+        this.last = newNode;
+        this.size++;
+
+    }
+
+    public T get(int index) {
+        // проверяю что не выхожу за пределы списка
+        // можно убрать проверку и получать первый или последний элемент списка, но это такое себе..
+        if (index < 0 || index >= this.size)
+            throw new IndexOutOfBoundsException();
+
+        // оптимизация для концов
+        if (index == 0)
+            return this.first.data;
+        if (index == this.size - 1)
+            return this.last.data;
+
+        // разматываю с начала списка
+        Node<T> currentNode = this.first;
+        int i = 0;
+        while (i < index && currentNode.hasNext()) {
+            currentNode = currentNode.next;
+            i++;
+        }
+        return currentNode.data;
     }
 
     public int size() {
-        int size = 0;
-
-        if (root == null)
-            return 0;
-        else {
-            size = 1;
-
-            Node current = root;
-            while (current.next != null) {
-                size++;
-                current = current.next;
-            }
-        }
-
-        return size;
-    }
-
-
-    Node findLast() {
-        if (root == null)
-            return null;
-        else {
-            Node current = root;
-
-            while (current.next != null) {
-                current = current.next;
-            }
-
-            return current;
-        }
+        return this.size;
     }
 }
