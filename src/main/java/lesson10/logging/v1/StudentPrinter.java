60ec5c37b88a0ac6f4763c2aab565036847148a0
Goodwin <yugin.sa@gmail.com>
2020-12-22 18:10:44 +0300

    добавил зависимости slf4j + logback в pom.xml и логгеры в классы-принтеры

--- src/main/java/lesson10/logging/v1/StudentPrinter.java
+++ src/main/java/lesson10/logging/v1/StudentPrinter.java
@@ -1,19 +1,27 @@
 package lesson10.logging.v1;
 
 import lesson10.logging.Student;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.List;
 
 public class StudentPrinter {
 
+    private static final Logger logger = LoggerFactory.getLogger(StudentPrinter.class);
+
     public void printAllNotPresentStudents(List<Student> students) {
         //TODO Напечатать список всех студентов, которые отсутствовали на тесте (нет оценки) с уровнем warn
+        students.stream().filter(s -> s.getResult() == null).forEach(s -> logger.warn(s.toString()));
     }
 
     public void printAllStudents(List<Student> students) {
         //TODO Напечатать список всех студентов с уровнем info
+        students.forEach(s -> logger.info(s.toString()));
     }
 
     public void printAllStudentsWith2(List<Student> students) {
         //TODO Напечатать список всех студентов, которые имеют оценку 2 c уровнем error
+        students.stream().filter(s -> s.getResult() != null && s.getResult() == 2).forEach(s -> logger.error(s.toString()));
     }
 }
